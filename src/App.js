60171899eca709a7cbd9c8473bc88cee37cb255b
Jibbyworks <JDBucog@yahoo.com>
2021-09-29 23:30:48 -0700

    add fetch functionality on load and on click

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react"
 import {
   FaEnvelopeOpen,
   FaUser,
@@ -6,11 +6,118 @@ import {
   FaMap,
   FaPhone,
   FaLock,
-} from 'react-icons/fa'
-const url = 'https://randomuser.me/api/'
-const defaultImage = 'https://randomuser.me/api/portraits/men/75.jpg'
+} from "react-icons/fa"
+const url = "https://randomuser.me/api/"
+const defaultImage = "https://randomuser.me/api/portraits/men/75.jpg"
+
 function App() {
-  return <h2>random user starter</h2>
+  const [loading, setLoading] = useState(true)
+  const [person, setPerson] = useState(null)
+  const [title, setTitle] = useState("name")
+  const [value, setValue] = useState("...loading")
+
+  const getPerson = async () => {
+    const response = await fetch("https://randomuser.me/api/")
+    const data = await response.json()
+    const person = data.results[0]
+    const { phone, email } = person
+    const { large: image } = person.picture
+    const {
+      login: { password },
+    } = person
+    const { first, last } = person.name
+    const {
+      dob: { age },
+    } = person
+    const {
+      street: { number, name },
+    } = person.location
+    const newPerson = {
+      email,
+      image,
+      age,
+      password,
+      phone,
+      street: `${number} ${name}`,
+      name: `${first} ${last}`,
+    }
+    setPerson(newPerson)
+    setLoading(false)
+    setTitle("name")
+    setValue(newPerson.name)
+  }
+
+  useEffect(() => {
+    getPerson()
+  }, [])
+
+  const handleValue = (e) => {
+    console.log(e.target)
+  }
+
+  return (
+    <main>
+      <div className="block bcg-black"></div>
+      <div className="block">
+        <div className="container">
+          <img
+            src={(person && person.image) || defaultImage}
+            alt="random user"
+            className="user-img"
+          />
+          <p className="user-title">my {title} is </p>
+          <p className="user-value">{value}</p>
+          <div className="values-list">
+            <buttons
+              className="icon"
+              data-label="name"
+              onMouseOver={handleValue}
+            >
+              <FaUser />
+            </buttons>
+            <buttons
+              className="icon"
+              data-label="email"
+              onMouseOver={handleValue}
+            >
+              <FaEnvelopeOpen />
+            </buttons>
+            <buttons
+              className="icon"
+              data-label="age"
+              onMouseOver={handleValue}
+            >
+              <FaCalendarTimes />
+            </buttons>
+            <buttons
+              className="icon"
+              data-label="street"
+              onMouseOver={handleValue}
+            >
+              <FaMap />
+            </buttons>
+            <buttons
+              className="icon"
+              data-label="phone"
+              onMouseOver={handleValue}
+            >
+              <FaPhone />
+            </buttons>
+            <buttons
+              className="icon"
+              data-label="password"
+              onMouseOver={handleValue}
+            >
+              <FaLock />
+            </buttons>
+          </div>
+          <button className="btn" type="button" onClick={getPerson}>
+            {loading ? "loading..." : "random user"}
+          </button>
+        </div>
+      </div>
+    </main>
+  )
 }
 
 export default App
